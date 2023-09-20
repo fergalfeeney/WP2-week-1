40fb767378a8488b7631897a4c9e9368aa68001b
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-09-20 16:42:55 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WP2-week1
+# WP2-week-1
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let contacts = [];
+
+app.post('/contacts', (req, res) => {
+  const contact = req.body;
+  console.log(contact); // to check what was received
+  contacts.push(contact);
+  res.send('Contact has been added to the database');
+  console.log(`Contact name is ${contact.name}. Number of contacts is ${contacts.length}`);
+});
+
+app.get('/contacts', (req, res) => {
+  res.send(contacts);
+});
+
+  app.get('/contacts/:id', (req,res) => { 
+
+ 
+
+         let id = req.params.id; 
+    
+          res.json(contacts[id]); 
+    
+      }) 
+
+     app.delete('/contact/:id',(req, res) => 
+
+ { 
+
+    let id = req.params.id;  
+    console.log(`removing contact ${contact[id].name}`) 
+    contact.splice(req.params.id, 1); 
+    res.send(contact); 
+
+ 
+
+  }) 
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
